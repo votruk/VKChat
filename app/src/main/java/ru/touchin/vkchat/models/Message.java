daf6974d78f03a73f67fe89b9ef295304ecd7e3b
ilia.kurtov <ilia.kurtov@touchin.ru>
2015-11-03 13:34:18 +0300

    Delete unused code

--- app/src/main/java/ru/touchin/vkchat/models/Message.java
+++ app/src/main/java/ru/touchin/vkchat/models/Message.java
@@ -1,41 +1,29 @@
 package ru.touchin.vkchat.models;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.api.client.util.Key;
 
 import org.zuzuk.utils.serialization.json.ObjectFromJson;
 
 public class Message extends ObjectFromJson{
-    @Key("uid")
-    @JsonProperty("uid")
+    @Key("id")
     private Long mId;
 
-    @Key("mid")
-    @JsonProperty("mid")
-    private Long mMineId;
-
     @Key("date")
-    @JsonProperty("date")
     private Long mDate;
 
     @Key("out")
-    @JsonProperty("out")
     private Integer isMessageMine;
 
     @Key("user_id")
-    @JsonProperty("user_id")
     private Long mUserId;
 
     @Key("from_id")
-    @JsonProperty("from_id")
     private Long mFromUserId;
 
     @Key("read_state")
-    @JsonProperty("read_state")
     private Integer mReadState;
 
     @Key("body")
-    @JsonProperty("body")
     private String mBody;
 
 
